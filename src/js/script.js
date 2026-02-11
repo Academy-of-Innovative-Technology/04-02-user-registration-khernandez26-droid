8932d7471fe18a45cfa529209046b565d7d4f2c6
Keilah <Khernandez26@aoiths.org>
2026-02-11 13:53:17 -0500

    paste

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,61 @@ document.querySelector(".btn-primary").addEventListener("click", function (event
     const firstName = document.querySelector("");
 
 });
+
+
+
+
+FN
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
+
+
+LN
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
+
+
+E
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
+
+
+P
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
+
+
+C
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
+
+
+AT
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
+
+
+AY
+document.querySelector("").addEventListener("click", function (event) {
+
+    const firstName = document.querySelector("");
+
+});
