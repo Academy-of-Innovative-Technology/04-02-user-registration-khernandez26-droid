b35da55be2179917f1aebb56d9604e8a84529025
Keilah <Khernandez26@aoiths.org>
2026-03-12 13:40:22 -0400

    completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,63 +1,86 @@
 document.querySelector(".btn-primary").addEventListener("click", function (event) {
 
-    const firstName = document.querySelector("");
-c
+    const btn = document.querySelector("#button");
+
+    btn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const emailInput = document.querySelector("#email");
+        const savedEmail = document.querySelector("#savedEmail");
+
+        savedEmail.textContent = emailInput.value;
+    });
 });
 
 
 
 
 FN
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-    const emailInput = document.querySelector("#email");
+    const firstName = document.querySelector("#firstName");
+const savedFirstName = document.querySelector("#savedFirstName");
 
+    savedFirstName.textContent = firstName.value;
 });
 
 
 LN
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-    const emailInput = document.querySelector("#email");
+    const lastNameInput = document.querySelector("#lastName");
+    const savedLastName = document.querySelector("#savedLastName");
 
+    savedLastName.textContent = lastNameInput.value;
 });
 
 
 E
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-   const passwordInput = document.querySelector("#password");
+   const emailInput = document.querySelector("#email");
+   const savedEmail = document.querySelector("#savedEmail");
 
+   savedEmail.textContent = emailInput.value;
 });
 
 
 P
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-  const countrySelect = document.querySelector("#country");
+  const passwordInput = document.querySelector("#password");
+  const savedPassword = document.querySelector("#savedPassword");
 
+  savedPassword.textContent = passwordInput.value;
 });
 
 
 C
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-    const accountSelect = document.querySelector("#account");
+    const countrySelect = document.querySelector("#country");
+    const savedCountry = document.querySelector("#savedCountry");
 
+    savedCountry.textContent = countrySelect.value;
 });
 
 
 AT
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-   const aboutTextarea = document.querySelector("#about");
+   const accountSelect = document.querySelector("#account");
+   const savedAccount = document.querySelector("#savedAccount");
 
+   savedAccount.textContent = accountSelect.value;
 });
 
 
 AY
-document.querySelector("").addEventListener("click", function (event) {
+document.querySelector("#button").addEventListener("click", function (event) {
 
-    const form = document.querySelector("#registrationForm");
+const aboutYouInput = document.querySelector("#aboutYou");
+const savedAboutYou = document.querySelector("#savedAboutYou");
 
+savedAboutYou.textContent = aboutYouInput.value;
 });
+
